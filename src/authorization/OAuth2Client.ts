68cad40650ebf212cab15b845c8dae56be70f1cd
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2023-06-05 12:34:18 +0530

    feat: implemented disconnect() for workspace

--- src/authorization/OAuth2Client.ts
+++ src/authorization/OAuth2Client.ts
@@ -8,6 +8,7 @@ import {
 } from "@rocket.chat/apps-engine/definition/accessors";
 import { IOAuth2Client } from "../../definition/authorization/IOAuthClient";
 import { IUser } from "@rocket.chat/apps-engine/definition/users";
+import { OAuth2Storage } from "./OAuth2Storage";
 import { sendNotification } from "../helper/message";
 import { getCredentials } from "../helper/getCredential";
 import { OAuth2Credential, OAuth2Locator } from "../../enum/OAuth2";
@@ -45,7 +46,34 @@ export class OAuth2Client implements IOAuth2Client {
         modify: IModify,
         http: IHttp,
         persis: IPersistence
-    ) {}
+    ) {
+        const persistenceRead = read.getPersistenceReader();
+        const oAuthStorage = new OAuth2Storage(persis, persistenceRead);
+        const { blockBuilder, elementBuilder } = this.app.getUtils();
+
+        const room = context.getRoom();
+        const user = context.getSender();
+        const userId = user.id;
+        const tokenInfo = await oAuthStorage.getCurrentWorkspace(userId);
+
+        if (tokenInfo) {
+            await oAuthStorage.disconnectUserFromCurrentWorkspace(userId);
+            const message = `you are being disconnected from the Workspace **${tokenInfo.workspace_name}**`;
+            await sendNotification(read, modify, user, room, { message });
+            return;
+        }
+
+        const authorizationUrl = await this.getAuthorizationUrl(user, read);
+        const message = `you are not connected to workspace!`;
+        const blocks = await getConnectBlock(
+            this.app,
+            message,
+            authorizationUrl
+        );
+        await sendNotification(read, modify, user, room, {
+            blocks,
+        });
+    }
 
     public async getAuthorizationUrl(
         user: IUser,
