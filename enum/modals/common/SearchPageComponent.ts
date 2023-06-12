7570abb502d0048945f5f6b2cefa1bd48b69c559
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2023-06-12 19:09:22 +0530

    fix: searchPageComponent label to look alike mandatory field

--- enum/modals/common/SearchPageComponent.ts
+++ enum/modals/common/SearchPageComponent.ts
@@ -2,5 +2,5 @@ export enum SearchPage {
     PLACEHOLDER = "Select a Page",
     BLOCK_ID = "search-page-component-block-id",
     ACTION_ID = "search-page-component-action-id",
-    LABEL = "Search For Page"
+    LABEL = "Search For Page *"
 }
