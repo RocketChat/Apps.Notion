6cf7b5362b1ec81980161e9f0a2e25aace3c2164
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2023-06-14 22:21:15 +0530

    fix: searchPageComponent to fetch Pages only Once during Interaction

--- src/modals/common/searchPageComponent.ts
+++ src/modals/common/searchPageComponent.ts
@@ -3,18 +3,28 @@ import { InputBlock } from "@rocket.chat/ui-kit";
 import { Error } from "../../../errors/Error";
 import { SearchPage } from "../../../enum/modals/common/SearchPageComponent";
 import { StaticSelectOptionsParam } from "../../../definition/ui-kit/Element/IStaticSelectElement";
+import { ModalInteractionStorage } from "../../storage/ModalInteraction";
+import { IPage } from "../../../definition/lib/INotion";
+import { ITokenInfo } from "../../../definition/authorization/IOAuth2Storage";
 export async function searchPageComponent(
     app: NotionApp,
-    token: string
+    modalInteraction: ModalInteractionStorage,
+    tokenInfo: ITokenInfo
 ): Promise<InputBlock | Error> {
     const { NotionSdk, elementBuilder, blockBuilder } = app.getUtils();
-    const response = await NotionSdk.searchPages(token);
+    const { access_token, workspace_id } = tokenInfo;
+    let pagesOrDatabases;
+    pagesOrDatabases = await modalInteraction.getPagesOrDatabase(workspace_id);
+    if (!pagesOrDatabases) {
+        pagesOrDatabases = await NotionSdk.searchPages(access_token);
+        if (pagesOrDatabases instanceof Error) {
+            return pagesOrDatabases;
+        }
 
-    if (response instanceof Error) {
-        return response;
+        await modalInteraction.storePagesOrDatabase(pagesOrDatabases, workspace_id);
     }
 
-    const accesiblePages = response;
+    const accesiblePages: Array<IPage> = pagesOrDatabases;
     const options: StaticSelectOptionsParam = accesiblePages.map((page) => {
         const text = page.name;
         const value = page.parent.page_id;
