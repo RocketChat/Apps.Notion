ac4858f5ce0de62b91310c46def26e4060ab5a47
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2023-07-07 12:03:10 +0530

    fix(helper): message to have guide for /notion commet command

--- enum/messages.ts
+++ enum/messages.ts
@@ -1,6 +1,7 @@
 export enum Messages {
     HELPER_COMMANDS = `• use \`/notion connect\` to connect your workspace   
         • use \`/notion disconnect\` to disconnect workspace   
+        • use \`/notion comment\` to comment on notion page   
         • use \`/notion create database\` to create database
         `,
     HELPER_TEXT = `:wave: Need some help with \`/notion\`?`,
