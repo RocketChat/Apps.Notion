93a9436c3f61660d58a158ab30e93afcaf2beb0f
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2023-07-25 16:56:08 +0530

    feat: created preserve(), getAll(), RemoveDisconnect() for Workspace

--- src/authorization/OAuth2Storage.ts
+++ src/authorization/OAuth2Storage.ts
@@ -36,6 +36,7 @@ export class OAuth2Storage implements IOAuth2Storage {
             true
         );
 
+        await this.preserveNewWorkspace(tokenInfo, userId);
         return;
     }
 
@@ -71,6 +72,106 @@ export class OAuth2Storage implements IOAuth2Storage {
                 ),
             ]
         )) as ITokenInfo[];
+
+        await this.removeDisconnectedWorkspace(removedTokenInfo, userId);
         return removedTokenInfo;
+    }
+
+    public async getAllConnectedWorkspaces(
+        userId: string
+    ): Promise<ITokenInfo[] | undefined> {
+        const [tokenInfos] = (await this.persistenceRead.readByAssociations([
+            new RocketChatAssociationRecord( // user association
+                RocketChatAssociationModel.USER,
+                userId
+            ),
+            new RocketChatAssociationRecord(
+                RocketChatAssociationModel.MISC,
+                NotionTokenType.ALL_CONNECTED_WORKSPACES
+            ),
+        ])) as Array<ITokenInfo[]>;
+
+        return tokenInfos;
+    }
+
+    public async preserveNewWorkspace(
+        tokenInfo: ITokenInfo,
+        userId: string
+    ): Promise<void> {
+        let workspaces = await this.getAllConnectedWorkspaces(userId);
+
+        if (workspaces) {
+            const index = workspaces.findIndex((workspace) => {
+                return workspace.workspace_id === tokenInfo.workspace_id;
+            });
+
+            if (index < 0) {
+                workspaces.push(tokenInfo);
+            } else {
+                workspaces[index] = tokenInfo;
+            }
+        } else {
+            workspaces = [tokenInfo];
+        }
+
+        await this.persistence.updateByAssociations(
+            [
+                new RocketChatAssociationRecord( // user association
+                    RocketChatAssociationModel.USER,
+                    userId
+                ),
+                new RocketChatAssociationRecord(
+                    RocketChatAssociationModel.MISC,
+                    NotionTokenType.ALL_CONNECTED_WORKSPACES
+                ),
+            ],
+            workspaces,
+            true
+        );
+    }
+
+    public async removeDisconnectedWorkspace(
+        tokenInfo: ITokenInfo,
+        userId: string
+    ): Promise<void> {
+        const workspaces = await this.getAllConnectedWorkspaces(userId);
+        const user_association = new RocketChatAssociationRecord( // user association
+            RocketChatAssociationModel.USER,
+            userId
+        );
+
+        if (!workspaces) {
+            return;
+        }
+
+        const filteredWorkspaces = workspaces.filter((workspace) => {
+            return workspace.workspace_id !== tokenInfo.workspace_id;
+        });
+
+        await this.persistence.updateByAssociations(
+            [
+                user_association,
+                new RocketChatAssociationRecord(
+                    RocketChatAssociationModel.MISC,
+                    NotionTokenType.ALL_CONNECTED_WORKSPACES
+                ),
+            ],
+            filteredWorkspaces,
+            true
+        );
+
+        if (filteredWorkspaces.length) {
+            await this.persistence.updateByAssociations(
+                [
+                    user_association,
+                    new RocketChatAssociationRecord(
+                        RocketChatAssociationModel.MISC,
+                        NotionTokenType.CURRENT_WORKSPACE
+                    ),
+                ],
+                filteredWorkspaces[filteredWorkspaces.length - 1],
+                true
+            );
+        }
     }
 }
