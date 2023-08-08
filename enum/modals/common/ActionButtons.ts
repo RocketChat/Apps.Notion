5055660dcb6554469a7a50c610f403c177570bfd
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2023-08-08 14:46:13 +0530

    feat: register send to page message action

--- enum/modals/common/ActionButtons.ts
+++ enum/modals/common/ActionButtons.ts
@@ -1,4 +1,6 @@
 export enum ActionButton {
     COMMENT_ON_PAGES_MESSAGE_BOX_ACTION = "comment-on-pages-message-box-action",
     COMMENT_ON_PAGES_MESSAGE_BOX_ACTION_LABEL = "CommentOnPagesLabel",
+    SEND_TO_PAGE_MESSAGE_ACTION = "send-to-page-message-action",
+    SEND_TO_PAGE_MESSAGE_ACTION_LABEL = "SendToPageLabel",
 }
