1acd7c508ee178210698e3bdf1e54b91fc68a257
Zishan Ahmad <zishan.barun@gmail.com>
2024-02-29 19:57:36 +0530

    changed subpage text back to page removed Parent Page text

--- enum/modals/common/SearchPageAndDatabaseComponent.ts
+++ enum/modals/common/SearchPageAndDatabaseComponent.ts
@@ -2,5 +2,5 @@ export enum SearchPageAndDatabase {
     PLACEHOLDER = "Select a Page or Database",
     BLOCK_ID = "search-page-and-database-component-block-id",
     ACTION_ID = "search-page-and-database-component-action-id",
-    LABEL = "Parent Page or Database Name *",
+    LABEL = "Page or Database Name *",
 }
